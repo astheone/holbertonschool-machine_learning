0c10b5839ccbd2ea7adfa884606eef23995997c9
astheone <sulajalvi10@gmail.com>
2025-10-19 18:41:49 +0000

    Add 11-the_western_exchange.py with np_transpose function

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""
+This module defines a function that returns
+the transpose of a numpy.ndarray.
+"""
+def np_transpose(matrix):
+    """Return the transpose of a numpy.ndarray"""
+    return matrix.T

1d2691f339ffd03849e816db1625f95961b9cd62
astheone <sulajalvi10@gmail.com>
2025-10-19 18:52:21 +0000

    Add 14-saddle_up.py with np_matmul function

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+This module defines a function that performs matrix multiplication
+on two numpy.ndarrays.
+"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """Return the matrix multiplication of two numpy.ndarrays"""
+    return np.matmul(mat1, mat2)

080cfdc1df93a34b3fc32c773b9c01e776f77eca
astheone <sulajalvi10@gmail.com>
2025-10-19 18:47:20 +0000

    Fix pycodestyle line length for task 12

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 """
 This module defines a function that performs element-wise
-addition, subtraction, multiplication, and division of two numpy.ndarrays.
+addition, subtraction, multiplication, and division of two
+numpy.ndarrays.
 """
 
 
 def np_elementwise(mat1, mat2):
-    """Return element-wise sum, difference, product and quotient of two numpy.ndarrays"""
+    """Return element-wise sum, difference, product and quotient
+    of two numpy.ndarrays"""
     add = mat1 + mat2
     sub = mat1 - mat2
     mul = mat1 * mat2

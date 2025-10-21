1478b34b8895c92b3754b42fc4639841036e0cc9
astheone <sulajalvi10@gmail.com>
2025-10-21 15:24:04 +0000

    Add solution to 17-integrate.py

---
+++ math/calculus/17-integrate.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""Module that calculates the integral of a polynomial"""
+
+
+def poly_integral(poly, C=0):
+    """
+    Calculates the integral of a polynomial.
+    Args:
+        poly (list): coefficients representing the polynomial
+        C (int, optional): integration constant
+    Returns:
+        list: coefficients of the integral of the polynomial
+    """
+    if not isinstance(poly, list) or len(poly) == 0 or not isinstance(C, (int, float)):
+        return None
+    if not all(isinstance(x, (int, float)) for x in poly):
+        return None
+
+    result = [C]
+    for i in range(len(poly)):
+        val = poly[i] / (i + 1)
+        if val.is_integer():
+            val = int(val)
+        result.append(val)
+
+    # Remove unnecessary trailing zeros
+    while len(result) > 1 and result[-1] == 0:
+        result.pop()
+
+    return result

a5274606cdf6cdaaf8378b9aebbe50da8a75f88c
astheone <sulajalvi10@gmail.com>
2025-10-21 10:15:11 +0000

    Add solution for 10-matisse.py

--- math/calculus/10-matisse.py
+++ math/calculus/10-matisse.py
@@ -12,20 +12,14 @@ def poly_derivative(poly):
     poly: list of coefficients (index = power of x)
     Returns a new list with the derivative coefficients.
     """
-    # kontrollo që poly të jetë listë me numra (int ose float)
-    if (not isinstance(poly, list) or
-            not all(isinstance(x, (int, float)) for x in poly)):
+    if not isinstance(poly, list) or len(poly) == 0:
         return None
-
-    # nëse është bosh ose me 1 element (si konstante)
-    if len(poly) <= 1:
+    if not all(isinstance(x, (int, float)) for x in poly):
+        return None
+    if len(poly) == 1:
         return [0]
 
-    # llogarit derivatën pa përdorur loops
     derivative = [i * poly[i] for i in range(1, len(poly))]
-
-    # nëse të gjithë elementët janë 0
-    if all(v == 0 for v in derivative):
+    if not derivative or all(v == 0 for v in derivative):
         return [0]
-
     return derivative

bcefc1e3ae347b3a5f3045c353ac372746c693aa
astheone <sulajalvi10@gmail.com>
2025-10-21 10:06:55 +0000

    Add solution for 9-sum_total

--- math/calculus/9-sum_total.py
+++ math/calculus/9-sum_total.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
+"""
+This module contains a function that calculates
+the summation of i squared from 1 to n.
+"""
+
 def summation_i_squared(n):
-    """Return the sum of squares from 1 to n"""
+    """
+    Return the sum of squares from 1 to n.
+    If n is not a valid positive integer, return None.
+    """
     if type(n) is not int or n < 1:
         return None
-    # Formula pa loops: (n * (n + 1) * (2n + 1)) / 6
     return int((n * (n + 1) * (2 * n + 1)) / 6)

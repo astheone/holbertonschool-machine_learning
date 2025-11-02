31ab9591c2886cd17afc63d3879d54a9b0810f07
astheone <sulajalvi10@gmail.com>
2025-11-02 17:10:32 +0000

    Fix import position for concat function

--- pipeline/pandas/11-concat.py
+++ pipeline/pandas/11-concat.py
@@ -3,11 +3,13 @@
 Concatenates two dataframes on their Timestamp index
 """
 
+import pandas as pd
+index = __import__('10-index').index
 
 def concat(df1, df2):
-    """Concatenate df2 and df1 using Timestamp as index"""
-    index = __import__('10-index').index
-
+    """
+    Concatenate df2 and df1 using Timestamp as index
+    """
     # 1. Vendos Timestamp si index për të dy dataframe-t
     df1 = index(df1)
     df2 = index(df2)
@@ -16,7 +18,6 @@ def concat(df1, df2):
     df2 = df2.loc[:1417411920]
 
     # 3. Bashko dataframe-t (df2 sipër, df1 poshtë)
-    import pandas as pd
     df = pd.concat([df2, df1], keys=['bitstamp', 'coinbase'])
 
     # 4. Kthe dataframe-n e bashkuar

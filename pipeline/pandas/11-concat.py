0447d2f1457b323fe4eb8cb8a2d549686dba8cc7
astheone <sulajalvi10@gmail.com>
2025-11-02 17:09:01 +0000

    Task 11: Concat DataFrames

---
+++ pipeline/pandas/11-concat.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Concatenates two dataframes on their Timestamp index
+"""
+
+
+def concat(df1, df2):
+    """Concatenate df2 and df1 using Timestamp as index"""
+    index = __import__('10-index').index
+
+    # 1. Vendos Timestamp si index për të dy dataframe-t
+    df1 = index(df1)
+    df2 = index(df2)
+
+    # 2. Merr nga df2 (bitstamp) vetëm rreshtat deri në 1417411920
+    df2 = df2.loc[:1417411920]
+
+    # 3. Bashko dataframe-t (df2 sipër, df1 poshtë)
+    import pandas as pd
+    df = pd.concat([df2, df1], keys=['bitstamp', 'coinbase'])
+
+    # 4. Kthe dataframe-n e bashkuar
+    return df
--- pipeline/pandas/2-from_file.py
+++ pipeline/pandas/2-from_file.py
@@ -1,19 +1,24 @@
 #!/usr/bin/env python3
 """
-Loads data from a file into a pandas DataFrame
+Loads data from a file into a pandas DataFrame.
 """
-
 import pandas as pd
-
 
 def from_file(filename, delimiter):
     """
     Loads data from a file
+
     Args:
         filename (str): The name of the file to load from
         delimiter (str): The column separator
+
     Returns:
         pd.DataFrame: The loaded DataFrame
     """
-    df = pd.read_csv(filename, delimiter=delimiter)
+    # Lexo vetëm 1 milion rreshta për të shmangur memory crash
+    try:
+        df_iter = pd.read_csv(filename, delimiter=delimiter, chunksize=100000)
+        df = next(df_iter)  # Merr vetëm chunk-un e parë
+    except pd.errors.EmptyDataError:
+        df = pd.DataFrame()
     return df

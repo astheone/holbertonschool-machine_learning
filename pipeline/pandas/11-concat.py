67eb233912a3f317fd61aab1997e431ee2ee16f3
astheone <sulajalvi10@gmail.com>
2025-11-02 17:11:36 +0000

    Fix pycodestyle spacing in concat

--- pipeline/pandas/11-concat.py
+++ pipeline/pandas/11-concat.py
@@ -6,6 +6,7 @@ Concatenates two dataframes on their Timestamp index
 import pandas as pd
 index = __import__('10-index').index
 
+
 def concat(df1, df2):
     """
     Concatenate df2 and df1 using Timestamp as index

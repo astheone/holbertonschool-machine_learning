ecc51267071191110ec05541d9cd9d0768f43022
astheone <sulajalvi10@gmail.com>
2025-11-04 12:20:21 +0000

    Add line graph plotting script

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,17 +1,11 @@
 #!/usr/bin/env python3
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
 
 
 def line():
-
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
-     
-    # your code here
-    plt.plot(y, color ='red')
+    plt.plot(np.arange(0, 11), y, 'r-')  # solid red line
     plt.xlim(0, 10)
     plt.show()
-    

193c38fb7f9d4157891f3a903df38d7fef5ac2b3
astheone <sulajalvi10@gmail.com>
2025-11-04 12:29:55 +0000

    Fix histogram plot and documentation for Project A

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -18,10 +18,9 @@ def frequency():
     student_grades = np.random.normal(68, 15, 50)
     plt.figure(figsize=(6.4, 4.8))
     plt.hist(student_grades, bins=range(0, 101, 10),
-             edgecolor='black', linewidth=1.0)
+             edgecolor='black')
     plt.title("Project A")
     plt.xlabel("Grades")
     plt.ylabel("Number of Students")
     plt.xlim(0, 100)
-    plt.ylim(0, 30)
     plt.show()

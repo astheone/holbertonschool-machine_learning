f8d8679e5e4c903df8ffcc2a194b4e6ae48fa893
astheone <sulajalvi10@gmail.com>
2025-11-04 12:41:02 +0000

    Fix colormap to terrain for Holberton checker

--- math/plotting/100-gradient.py
+++ math/plotting/100-gradient.py
@@ -20,3 +20,10 @@ def gradient():
     z = np.random.rand(2000) + 40 - np.sqrt(np.square(x) + np.square(y))
 
     plt.figure(figsize=(6.4, 4.8))
+    scatter = plt.scatter(x, y, c=z, cmap='terrain', s=20)
+    cbar = plt.colorbar(scatter)
+    cbar.set_label("elevation (m)")
+    plt.title("Mountain Elevation")
+    plt.xlabel("x coordinate (m)")
+    plt.ylabel("y coordinate (m)")
+    plt.show()

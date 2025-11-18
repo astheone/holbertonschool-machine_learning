838ec79f50ca7c720830c089a491a70888c04550
astheone <sulajalvi10@gmail.com>
2025-11-18 19:00:51 +0000

    Fix SWAPI passengers task

--- pipeline/apis/0-passengers.py
+++ pipeline/apis/0-passengers.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python3
 """
-Module that returns list of ships that can hold given number of passengers
-using the SWAPI API.
+Module that returns a list of starships that can hold a given number of passengers.
 """
 
 import requests
 
 def availableShips(passengerCount):
     """
-    Returns list of starships that can carry at least passengerCount passengers.
-    Handles pagination properly.
+    Returns a list of starships that can carry at least passengerCount passengers.
+    Handles pagination of the SWAPI API.
     """
     url = "https://swapi-api.alx-tools.com/api/starships/"
     result = []
@@ -21,11 +20,13 @@ def availableShips(passengerCount):
         for ship in data.get("results", []):
             passengers = ship.get("passengers", "0")
 
-            # Clean passengers value: remove commas, handle 'unknown', 'n/a', etc.
-            if passengers.isdigit() or passengers.replace(",", "").isdigit():
-                num = int(passengers.replace(",", ""))
+            # Clean passengers value: remove commas, ignore 'unknown', etc.
+            clean = passengers.replace(",", "")
+
+            if clean.isdigit():
+                num = int(clean)
                 if num >= passengerCount:
-                    result.append(ship["name"])
+                    result.append(ship.get("name"))
 
         url = data.get("next")  # pagination
 
---
+++ pipeline/apis/README.md
@@ -0,0 +1,3 @@
+# Holberton Machine Learning - API Tasks
+
+This project contains API tasks using the SWAPI API.

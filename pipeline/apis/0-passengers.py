db6f8e2cf553dbf040e495cfb14aa86908345305
astheone <sulajalvi10@gmail.com>
2025-11-18 19:01:56 +0000

    Fix pycodestyle for 0-passengers

--- pipeline/apis/0-passengers.py
+++ pipeline/apis/0-passengers.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
 """
-Module that returns a list of starships that can hold a given number of passengers.
+Module that returns a list of starships that can hold
+a given number of passengers.
 """
 
 import requests
 
+
 def availableShips(passengerCount):
     """
-    Returns a list of starships that can carry at least passengerCount passengers.
-    Handles pagination of the SWAPI API.
+    Returns a list of starships that can carry at least
+    passengerCount passengers. Handles SWAPI pagination.
     """
     url = "https://swapi-api.alx-tools.com/api/starships/"
     result = []
@@ -19,15 +21,9 @@ def availableShips(passengerCount):
 
         for ship in data.get("results", []):
             passengers = ship.get("passengers", "0")
-
-            # Clean passengers value: remove commas, ignore 'unknown', etc.
             clean = passengers.replace(",", "")
 
             if clean.isdigit():
                 num = int(clean)
                 if num >= passengerCount:
                     result.append(ship.get("name"))
-
-        url = data.get("next")  # pagination
-
-    return result

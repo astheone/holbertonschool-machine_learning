630c49e539b4d6f7c3b41f6968735e76822dc7bf
Alvi Sulaj <sulajalvi10@gmail.com>
2026-01-27 14:54:41 +0100

    Task 10: NeuralNetwork forward propagation implemented

---
+++ supervised_learning/classification/10-neural_network.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+"""
+Defines a neural network with one hidden layer performing binary classification
+"""
+
+import numpy as np
+
+
+class NeuralNetwork:
+    """
+    Neural network with one hidden layer
+    """
+
+    def __init__(self, nx, nodes):
+        """
+        nx: number of input features
+        nodes: number of nodes in hidden layer
+        """
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if not isinstance(nodes, int):
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+
+        # Hidden layer
+        self.__W1 = np.random.randn(nodes, nx)
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
+
+        # Output neuron
+        self.__W2 = np.random.randn(1, nodes)
+        self.__b2 = 0
+        self.__A2 = 0
+
+    # Getters
+    @property
+    def W1(self):
+        return self.__W1
+
+    @property
+    def b1(self):
+        return self.__b1
+
+    @property
+    def A1(self):
+        return self.__A1
+
+    @property
+    def W2(self):
+        return self.__W2
+
+    @property
+    def b2(self):
+        return self.__b2
+
+    @property
+    def A2(self):
+        return self.__A2
+
+    def forward_prop(self, X):
+        """
+        Performs forward propagation of the neural network
+        X: input data of shape (nx, m)
+        Returns: A1, A2
+        """
+        # Sigmoid helper
+        def sigmoid(z):
+            return 1 / (1 + np.exp(-z))
+
+        # Hidden layer
+        Z1 = np.dot(self.__W1, X) + self.__b1
+        self.__A1 = sigmoid(Z1)
+
+        # Output neuron
+        Z2 = np.dot(self.__W2, self.__A1) + self.__b2
+        self.__A2 = sigmoid(Z2)
+
+        return self.__A1, self.__A2

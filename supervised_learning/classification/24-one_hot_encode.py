b2a5f72f31a5346c67d5bfb9a28bc9d3f7fcd53b
Alvi Sulaj <sulajalvi10@gmail.com>
2026-01-27 15:45:04 +0100

    24. One-hot encoding function

---
+++ supervised_learning/classification/24-one_hot_encode.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+import numpy as np
+
+def one_hot_encode(Y, classes):
+    """
+    Converts a numeric label vector into a one-hot matrix.
+
+    Y: numpy.ndarray of shape (m,) containing numeric class labels
+    classes: maximum number of classes
+
+    Returns: one-hot encoded matrix of shape (classes, m)
+    """
+    if not isinstance(Y, np.ndarray) or not isinstance(classes, int):
+        return None
+    if Y.ndim != 1 or classes < 1:
+        return None
+
+    m = Y.shape[0]
+    one_hot = np.zeros((classes, m))
+    one_hot[Y, np.arange(m)] = 1
+    return one_hot

1011a9c78041870c8e8aeac7b6daa05ee9c7654a
Alvi Sulaj <sulajalvi10@gmail.com>
2026-01-27 15:28:10 +0100

    17. Privatize DeepNeuralNetwork with getters

---
+++ supervised_learning/classification/17-deep_neural_network.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+import numpy as np
+
+class DeepNeuralNetwork:
+    def __init__(self, nx, layers):
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if not isinstance(layers, list) or len(layers) == 0:
+            raise TypeError("layers must be a list of positive integers")
+        if not all(isinstance(n, int) and n > 0 for n in layers):
+            raise TypeError("layers must be a list of positive integers")
+        self.__L = len(layers)
+        self.__cache = {}
+        self.__weights = {}
+        for l in range(1, self.__L + 1):
+            nodes = layers[l - 1]
+            prev_nodes = nx if l == 1 else layers[l - 2]
+            he_std = np.sqrt(2 / prev_nodes)
+            self.__weights['W' + str(l)] = np.random.randn(nodes, prev_nodes) * he_std
+            self.__weights['b' + str(l)] = np.zeros((nodes, 1))
+
+    @property
+    def L(self):
+        return self.__L
+
+    @property
+    def cache(self):
+        return self.__cache
+
+    @property
+    def weights(self):
+        return self.__weights

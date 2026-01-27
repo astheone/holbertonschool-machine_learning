bd9267dce67cccf25edfd27e0731d8ad607efc29
Alvi Sulaj <sulajalvi10@gmail.com>
2026-01-27 15:18:47 +0100

    Fix He initialization in DeepNeuralNetwork

--- supervised_learning/classification/16-deep_neural_network.py
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -1,24 +1,28 @@
 #!/usr/bin/env python3
 import numpy as np
 
-
 class DeepNeuralNetwork:
-    """Deep neural network performing binary classification"""
+    """Defines a deep neural network performing binary classification"""
 
     def __init__(self, nx, layers):
+        # Validimi i nx
         if not isinstance(nx, int):
             raise TypeError("nx must be an integer")
         if nx < 1:
             raise ValueError("nx must be a positive integer")
+
+        # Validimi i layers
         if not isinstance(layers, list) or len(layers) == 0:
             raise TypeError("layers must be a list of positive integers")
         if not all(isinstance(n, int) and n > 0 for n in layers):
             raise TypeError("layers must be a list of positive integers")
 
-        self.L = len(layers)
-        self.cache = {}
-        self.weights = {}
+        # Inicializimi i atributëve publikë
+        self.L = len(layers)       # Numri i layers
+        self.cache = {}            # Dictionary për vlerat ndërmjetëse
+        self.weights = {}          # Dictionary për peshat dhe bias
 
+        # Loop vetëm një herë për të inicializuar peshat dhe bias
         for l in range(1, self.L + 1):
             nodes = layers[l - 1]
             prev_nodes = nx if l == 1 else layers[l - 2]

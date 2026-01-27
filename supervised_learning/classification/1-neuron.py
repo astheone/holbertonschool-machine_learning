7362aa63bc817cd1a075691fcc27067d40c083b8
Alvi Sulaj <sulajalvi10@gmail.com>
2026-01-27 14:42:38 +0100

    Add private attributes and getters to Neuron

---
+++ supervised_learning/classification/1-neuron.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+"""
+Defines a single neuron performing binary classification
+"""
+
+import numpy as np
+
+
+class Neuron:
+    """
+    Defines a neuron
+    """
+
+    def __init__(self, nx):
+        """
+        Initializes the neuron
+        """
+        if not isinstance(nx, int):
+            raise TypeError("nx must be a integer")
+        if nx < 1:
+            raise ValueError("nx must be positive")
+
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """
+        Getter for weights
+        """
+        return self.__W
+
+    @property
+    def b(self):
+        """
+        Getter for bias
+        """
+        return self.__b
+
+    @property
+    def A(self):
+        """
+        Getter for activated output
+        """
+        return self.__A

c41c0a1e4bff036a70a6648b2d238d6f14f7c897
Alvi Sulaj <sulajalvi10@gmail.com>
2026-01-27 15:06:40 +0100

    Create DeepNeuralNetwork class with He initialization

---
+++ supervised_learning/classification/16-deep_neural_network.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""16-deep_neural_network.py"""
+
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """Deep neural network performing binary classification"""
+
+    def __init__(self, nx, layers):
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if not isinstance(layers, list) or len(layers) == 0:
+            raise TypeError("layers must be a list of positive integers")
+        if not all(isinstance(n, int) and n > 0 for n in layers):
+            raise TypeError("layers must be a list of positive integers")
+
+        self.L = len(layers)
+        self.cache = {}
+        self.weights = {}
+
+        for l in range(1, self.L + 1):
+            nodes = layers[l - 1]
+            if l == 1:
+                he_std = np.sqrt(2 / nx)
+                self.weights['W1'] = np.random.randn(nodes, nx) * he_std
+            else:
+                he_std = np.sqrt(2 / layers[l - 2])
+                self.weights['W' + str(l)] = np.random.randn(nodes, layers[l - 2]) * he_std
+            self.weights['b' + str(l)] = np.zeros((nodes, 1))

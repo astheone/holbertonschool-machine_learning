b8edd20d7a191a9c6a8614ffb9494676b967a922
Alvi Sulaj <sulajalvi10@gmail.com>
2026-01-27 15:01:07 +0100

    Add gradient_descent method to NeuralNetwork

--- supervised_learning/classification/13-neural_network.py
+++ supervised_learning/classification/13-neural_network.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
-"""Module for NeuralNetwork gradient descent"""
-
+"""13. NeuralNetwork Gradient Descent"""
 import numpy as np
 
 
 class NeuralNetwork:
-    """Neural network with one hidden layer performing binary classification"""
+    """Defines a neural network with one hidden layer
+       performing binary classification
+    """
 
     def __init__(self, nx, nodes):
         if not isinstance(nx, int):
@@ -49,42 +50,44 @@ class NeuralNetwork:
         return self.__A2
 
     def forward_prop(self, X):
-        """Forward propagation using sigmoid activation"""
-        self.__A1 = 1 / (1 + np.exp(-(self.__W1 @ X + self.__b1)))
-        self.__A2 = 1 / (1 + np.exp(-(self.__W2 @ self.__A1 + self.__b2)))
+        """Calculates forward propagation of the neural network"""
+        Z1 = self.__W1 @ X + self.__b1
+        self.__A1 = 1 / (1 + np.exp(-Z1))
+        Z2 = self.__W2 @ self.__A1 + self.__b2
+        self.__A2 = 1 / (1 + np.exp(-Z2))
         return self.__A1, self.__A2
 
     def cost(self, Y, A):
-        """Compute cost using logistic regression"""
+        """Calculates the cost using logistic regression"""
         m = Y.shape[1]
-        log_cost1 = Y * np.log(A)
-        log_cost2 = (1 - Y) * np.log(1.0000001 - A)
-        cost = - (1 / m) * np.sum(log_cost1 + log_cost2)
+        cost = -(1 / m) * np.sum(
+            Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A)
+        )
         return cost
 
     def evaluate(self, X, Y):
-        """Evaluate predictions and compute cost"""
-        self.forward_prop(X)
-        predictions = np.where(self.__A2 >= 0.5, 1, 0)
-        cost = self.cost(Y, self.__A2)
-        return predictions, cost
+        """Evaluates predictions and returns prediction and cost"""
+        A1, A2 = self.forward_prop(X)
+        cost = self.cost(Y, A2)
+        prediction = np.where(A2 >= 0.5, 1, 0)
+        return prediction, cost
 
     def gradient_descent(self, X, Y, A1, A2, alpha=0.05):
-        """Performs one pass of gradient descent and updates weights and biases"""
+        """Performs one pass of gradient descent on the neural network"""
         m = Y.shape[1]
 
         # Output layer gradients
         dZ2 = A2 - Y
-        dW2 = (1 / m) * dZ2 @ A1.T
+        dW2 = (1 / m) * (dZ2 @ A1.T)
         db2 = (1 / m) * np.sum(dZ2, axis=1, keepdims=True)
 
         # Hidden layer gradients
         dZ1 = (self.__W2.T @ dZ2) * (A1 * (1 - A1))
-        dW1 = (1 / m) * dZ1 @ X.T
+        dW1 = (1 / m) * (dZ1 @ X.T)
         db1 = (1 / m) * np.sum(dZ1, axis=1, keepdims=True)
 
-        # Update parameters
-        self.__W1 -= alpha * dW1
-        self.__b1 -= alpha * db1
-        self.__W2 -= alpha * dW2
-        self.__b2 -= alpha * db2
+        # Update weights and biases
+        self.__W1 = self.__W1 - alpha * dW1
+        self.__b1 = self.__b1 - alpha * db1
+        self.__W2 = self.__W2 - alpha * dW2
+        self.__b2 = self.__b2 - alpha * db2

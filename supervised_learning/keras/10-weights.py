68b75890efe811c32ca7098a67117181232449d5
Alvi Sulaj <sulajalvi10@gmail.com>
2026-02-03 14:04:51 +0100

    Add task 10: Save and load weights

---
+++ supervised_learning/keras/10-weights.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""
+Module for saving and loading model weights
+"""
+import tensorflow.keras as K
+
+
+def save_weights(network, filename, save_format='keras'):
+    """
+    Saves a model's weights
+
+    Args:
+        network: the model whose weights should be saved
+        filename: path of the file that the weights should be saved to
+        save_format: format in which the weights should be saved
+
+    Returns:
+        None
+    """
+    network.save_weights(filename, save_format=save_format)
+
+
+def load_weights(network, filename):
+    """
+    Loads a model's weights
+
+    Args:
+        network: the model to which the weights should be loaded
+        filename: path of the file that the weights should be loaded from
+
+    Returns:
+        None
+    """
+    network.load_weights(filename)
+    

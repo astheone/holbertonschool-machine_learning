cb716c1191e18e59f5fa258314b0f15f757465f3
Alvi Sulaj <sulajalvi10@gmail.com>
2026-02-03 14:11:30 +0100

    Add test_model function

--- supervised_learning/keras/12-test.py
+++ supervised_learning/keras/12-test.py
@@ -1,21 +1,19 @@
-#!/usr/bin/env python3
-"""Test a neural network model"""
-
-import tensorflow.keras as K
-
+import numpy as np
 
 def test_model(network, data, labels, verbose=True):
     """
-    Tests a neural network
+    Tests a neural network.
 
     Args:
-        network: trained model to test
-        data: input data
-        labels: correct one-hot labels
-        verbose: print progress if True
+        network (keras.models.Model): The network model to test.
+        data (numpy.ndarray): The input data to test the model with.
+        labels (numpy.ndarray): The correct one-hot labels of the data.
+        verbose (bool, optional): Determines if output should be printed during the testing process. Defaults to True.
 
     Returns:
-        loss, accuracy
+        tuple: The loss and accuracy of the model with the testing data, respectively.
     """
+    # Test the model on the input data and labels
     loss, accuracy = network.evaluate(data, labels, verbose=verbose)
+
     return loss, accuracy

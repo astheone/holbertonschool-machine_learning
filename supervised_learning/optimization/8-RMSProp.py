93c1da8b6c8426033ae970c9ccf52ffbc4368b82
Alvi Sulaj <sulajalvi10@gmail.com>
2026-02-03 17:40:22 +0100

    Add RMSProp optimizer function

---
+++ supervised_learning/optimization/8-RMSProp.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""RMSProp optimization algorithm"""
+import tensorflow as tf
+
+
+def create_RMSProp_op(alpha, beta2, epsilon):
+    """
+    Creates RMSProp optimization operation in TensorFlow
+    
+    Args:
+        alpha: learning rate
+        beta2: RMSProp weight (discounting factor)
+        epsilon: small number to avoid division by zero
+    
+    Returns:
+        optimizer: TensorFlow RMSProp optimizer
+    """
+    optimizer = tf.keras.optimizers.RMSprop(
+        learning_rate=alpha,
+        rho=beta2,
+        epsilon=epsilon
+    )
+    return optimizer

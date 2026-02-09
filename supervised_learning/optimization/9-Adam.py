c2c315c9eb2195697b171ed0f0eee3a7107984e8
Alvi Sulaj <sulajalvi10@gmail.com>
2026-02-09 12:00:05 +0100

    Add Adam optimization implementation

--- supervised_learning/optimization/9-Adam.py
+++ supervised_learning/optimization/9-Adam.py
@@ -24,17 +24,12 @@ def update_variables_Adam(alpha, beta1, beta2, epsilon, var, grad, v, s, t):
     """
     # Calculate first moment (momentum)
     v_new = beta1 * v + (1 - beta1) * grad
-    
     # Calculate second moment (RMSprop)
     s_new = beta2 * s + (1 - beta2) * (grad ** 2)
-    
     # Bias correction for first moment
     v_corrected = v_new / (1 - beta1 ** t)
-    
     # Bias correction for second moment
     s_corrected = s_new / (1 - beta2 ** t)
-    
     # Update variable
     var_updated = var - alpha * v_corrected / (np.sqrt(s_corrected) + epsilon)
-    
     return var_updated, v_new, s_new

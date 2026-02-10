3df8f4d76dea50dab306cd47ecb5f65ee9aab382
Alvi Sulaj <sulajalvi10@gmail.com>
2026-02-10 14:50:48 +0100

    Add moving_average function

---
+++ supervised_learning/optimization/4-moving_average.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Moving Average"""
+
+
+def moving_average(data, beta):
+    """Calculates the weighted moving average of a data set with bias correction"""
+    moving_averages = []
+    v = 0
+
+    for i, x in enumerate(data):
+        v = beta * v + (1 - beta) * x
+        bias_correction = 1 - beta ** (i + 1)
+        moving_averages.append(v / bias_correction)
+
+    return moving_averages

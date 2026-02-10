a264d086b36c81c6d5099b8bd654101ffa751a7b
Alvi Sulaj <sulajalvi10@gmail.com>
2026-02-10 14:55:46 +0100

    Add learning_rate_decay TensorFlow function

---
+++ supervised_learning/optimization/12-learning_rate_decay.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Learning Rate Decay Upgraded"""
+import tensorflow as tf
+
+
+def learning_rate_decay(alpha, decay_rate, decay_step):
+    """Creates a learning rate decay operation in TensorFlow
+    using inverse time decay"""
+    return tf.keras.optimizers.schedules.InverseTimeDecay(
+        initial_learning_rate=alpha,
+        decay_steps=decay_step,
+        decay_rate=decay_rate,
+        staircase=True
+    )

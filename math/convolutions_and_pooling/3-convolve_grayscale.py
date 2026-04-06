053fff77bf0d952f9b6866aeed724cbf82c0cbbb
Alvi Sulaj <sulajalvi10@gmail.com>
2026-04-07 01:44:22 +0200

    Add strided convolution on grayscale images

---
+++ math/convolutions_and_pooling/3-convolve_grayscale.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""Strided Convolution"""
+import numpy as np
+
+
+def convolve_grayscale(images, kernel, padding='same', stride=(1, 1)):
+    """Performs a convolution on grayscale images"""
+    m, h, w = images.shape
+    kh, kw = kernel.shape
+    sh, sw = stride
+
+    if padding == 'same':
+        ph = (kh - 1) // 2
+        pw = (kw - 1) // 2
+    elif padding == 'valid':
+        ph, pw = 0, 0
+    else:
+        ph, pw = padding
+
+    oh = (h + 2 * ph - kh) // sh + 1
+    ow = (w + 2 * pw - kw) // sw + 1
+    padded = np.pad(images, ((0, 0), (ph, ph), (pw, pw)), mode='constant')
+    output = np.zeros((m, oh, ow))
+
+    for i in range(oh):
+        for j in range(ow):
+            output[:, i, j] = np.sum(
+                padded[:, i*sh:i*sh+kh, j*sw:j*sw+kw] * kernel,
+                axis=(1, 2)
+            )
+    return output

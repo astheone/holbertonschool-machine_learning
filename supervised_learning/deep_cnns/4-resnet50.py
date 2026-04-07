ba05b5fc847d3c89b7d5ae1c97638cf1efcb61cd
Alvi Sulaj <sulajalvi10@gmail.com>
2026-04-07 02:15:22 +0200

    Add ResNet-50 architecture

---
+++ supervised_learning/deep_cnns/4-resnet50.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+"""ResNet-50"""
+from tensorflow import keras as K
+identity_block = __import__('2-identity_block').identity_block
+projection_block = __import__('3-projection_block').projection_block
+
+
+def resnet50():
+    """Builds the ResNet-50 architecture"""
+    init = K.initializers.HeNormal(seed=0)
+    X = K.Input(shape=(224, 224, 3))
+
+    conv1 = K.layers.Conv2D(
+        64, (7, 7), strides=(2, 2), padding='same', kernel_initializer=init
+    )(X)
+    bn1 = K.layers.BatchNormalization(axis=3)(conv1)
+    act1 = K.layers.Activation('relu')(bn1)
+    pool1 = K.layers.MaxPooling2D(
+        (3, 3), strides=(2, 2), padding='same'
+    )(act1)
+
+    x = projection_block(pool1, [64, 64, 256], s=1)
+    x = identity_block(x, [64, 64, 256])
+    x = identity_block(x, [64, 64, 256])
+
+    x = projection_block(x, [128, 128, 512])
+    x = identity_block(x, [128, 128, 512])
+    x = identity_block(x, [128, 128, 512])
+    x = identity_block(x, [128, 128, 512])
+
+    x = projection_block(x, [256, 256, 1024])
+    x = identity_block(x, [256, 256, 1024])
+    x = identity_block(x, [256, 256, 1024])
+    x = identity_block(x, [256, 256, 1024])
+    x = identity_block(x, [256, 256, 1024])
+    x = identity_block(x, [256, 256, 1024])
+
+    x = projection_block(x, [512, 512, 2048])
+    x = identity_block(x, [512, 512, 2048])
+    x = identity_block(x, [512, 512, 2048])
+
+    avg = K.layers.AveragePooling2D((7, 7), strides=(1, 1))(x)
+    output = K.layers.Dense(
+        1000, activation='softmax', kernel_initializer=init
+    )(avg)
+
+    return K.Model(inputs=X, outputs=output)
